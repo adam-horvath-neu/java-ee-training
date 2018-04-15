aed7c41ed36a152ecaba81a34011523de9220045
Szilágyi István <isti1350@gmail.com>
2018-04-15 22:55:39 +0200

    fin

--- maven-multimodule/maven-multimodule-booking-core/src/main/java/hu/training/booking/merchtypes/SzallasHU.java
+++ maven-multimodule/maven-multimodule-booking-core/src/main/java/hu/training/booking/merchtypes/SzallasHU.java
@@ -5,9 +5,7 @@ import hu.training.booking.tckt.Ticket;
 
 public class SzallasHU extends Merchant {
 
-	/* (non-Javadoc)
-	 * @see hu.training.authenticate.booking.stm.MerchPrint#printMessage()
-	 */
+
 	public void printMessage(Ticket ticket) {
 		System.out.println("Your payment is successful. Thank you for booking tickets with Szallas.hu and hope to see you again!!!");
 		
