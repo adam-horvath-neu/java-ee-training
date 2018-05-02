9d68ac705f9f7db5b3fa087df26951cda1ee71ac
Gunti017 <guntikee017@gmail.com>
2018-05-02 13:45:26 +0200

    Login_homework

---
+++ login_HomeWork/example-service/src/main/java/hu/neuron/service/impl/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package hu.neuron.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hu.neuron.core.dao.UserDao;
+import hu.neuron.core.dao.impl.UserDaoImpl;
+import hu.neuron.core.dto.UserDto;
+import hu.neuron.service.UserService;
+import hu.neuron.service.converter.UserConverter;
+import hu.neuron.service.vo.UserVo;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserVo registration(UserVo vo) {
+		UserDao userDao = new UserDaoImpl();
+
+		Long id = userDao.save(UserConverter.toUserDto(vo));
+		// userDao.find(id);
+		return vo;
+	}
+
+	@Override
+	public void delete(Long id) {
+		UserDao userDao = new UserDaoImpl();
+		userDao.delete(id);
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see hu.neuron.service.UserService#findByUserName(java.lang.String)
+	 */
+	@Override
+	public UserVo findByUserName(String userName) {
+		UserDao userDao = new UserDaoImpl();
+		
+		return  UserConverter.toUserVo(userDao.findByName(userName));
+	}
+
+	@Override
+	public List<UserVo> findAllElements() {
+		UserDao userDao = new UserDaoImpl();
+		List<UserVo> userVo = new ArrayList<UserVo>();
+		List<UserDto> userDaoList= userDao.findAll();
+		for(int i =0; i< userDaoList.size(); i++) {
+			userVo.add(UserConverter.toUserVo(userDaoList.get(i)));
+			
+		}
+		return userVo;
+	}
+
+	@Override
+	public void upadte(UserVo t) {
+		UserDao userDao = new UserDaoImpl();
+		userDao.upadte(UserConverter.toUserDto(t));
+		
+	}
+	
+	
+
+}
---
+++ login_HomeWork/example-web/src/main/java/hu/neuron/web/servlet/Registration.java
@@ -0,0 +1,141 @@
+package hu.neuron.web.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import hu.neuron.service.UserService;
+import hu.neuron.service.impl.UserServiceImpl;
+import hu.neuron.service.vo.UserVo;
+
+
+@WebServlet("/registration")
+public class Registration extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Registration() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		UserService service = new UserServiceImpl();
+		String op = request.getParameter("op");
+		String password = request.getParameter("password");
+		String password2 = request.getParameter("passwordagain");
+		if (password != null && !password.equals(password2)) {
+			request.setAttribute("ERROR", "Jelszó nem egyezik!");
+		}
+		else {
+		UserVo vo = new UserVo();
+		vo.setUsername(request.getParameter("username"));
+		vo.setPassword(password);
+		vo.setFirstname(request.getParameter("firstname"));
+		vo.setLastname(request.getParameter("lastname"));
+		vo.setEmail(request.getParameter("email"));
+		vo.setPhone(request.getParameter("phone"));
+		
+		
+		if(op.equals("add"))			
+		
+			service.registration(vo);
+		}
+		if(op.equals("del")) {
+			Long id = Long.parseLong(request.getParameter("did"));
+			service.delete(id);
+		}
+		if(op.equals("update")) {
+			UserVo vo = new UserVo();
+			vo.setUsername(request.getParameter("username"));
+			vo.setPassword(request.getParameter("password"));
+			vo.setFirstname(request.getParameter("firstname"));
+			vo.setLastname(request.getParameter("lastname"));
+			vo.setEmail(request.getParameter("email"));
+			vo.setPhone(request.getParameter("phone"));
+			service.upadte(vo);
+			 
+		}
+		
+		
+		
+
+		request.setAttribute("OK", Boolean.TRUE);
+
+		request.getRequestDispatcher("registration.jsp").forward(request, response);
+//		}}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String op = req.getParameter("op");
+		UserService service = new UserServiceImpl();
+		
+		
+
+		
+		if (op.equals("get")) {
+			List<UserVo> list = new ArrayList<UserVo>();			
+			list =service.findAllElements();
+			if(list.isEmpty()) {
+				}
+			else {
+			Response rv = new Response(list);
+			Gson gson = new Gson();
+			resp.reset();
+			resp.setCharacterEncoding("UTF-8");
+			
+			gson.toJson(rv, resp.getWriter());
+			}
+			resp.getWriter().close();
+		}
+	
+		
+		
+		
+	}
+
+	public static class Response {
+		private List<UserVo> data;
+
+		public Response(List<UserVo> data) {
+			super();
+			this.data = data;
+		}
+
+		public List<UserVo> getData() {
+			return data;
+		}
+
+		public void setData(List<UserVo> data) {
+			this.data = data;
+		}
+
+	}
+
+	
+	
+	
+}
